a350c43e372d39ac056a9247b315086ca48d20e3
JavGog <javierggjavier@gmail.com>
2022-12-21 20:53:40 +0000

    Adding user and password for connecting data base

--- credentials.php
+++ credentials.php
@@ -6,8 +6,8 @@
 
 // TODO: Change these to match your own TT284 database credentials
 // Note that it is important to use single quotes for the password variable
-$database_user = 'oucuhere';
-$database_password = 'PASSWORDHERE';
+$database_user = 'zy958678';
+$database_password = '7nHERck!2sJ%V#9#';
 
 // OPTIONAL: Edit these lines if you're using another database
 $database_host = 'localhost';
